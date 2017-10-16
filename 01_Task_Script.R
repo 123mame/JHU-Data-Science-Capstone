e0cde76a6df3c829d6671393713f2b631b53ab38
Mark Blackmore <mblackmo@yahoo.com>
2017-10-16 11:21:31 -0700

    Working versions of document term matrix, word distribution, word cloud.

--- 01_Task_Script.R
+++ 01_Task_Script.R
@@ -8,7 +8,10 @@
 library(downloader)
 library(tm)
 library(knitr)
+library(tidyverse)
 library(dplyr)
+library(data.table)
+library(ggthemes)
 
 #' ## 1. Download and explore the data
 #'
@@ -28,62 +31,64 @@ news_file    <- "./data/final/en_US/en_US.news.txt"
 twitter_file <- "./data/final/en_US/en_US.twitter.txt"  
 
 #' File Sizes (Mb)
-blogs_size <- file.size(blogs_file) / (2^20)
-news_size  <- file.size(news_file) / (2^20)
+blogs_size   <- file.size(blogs_file) / (2^20)
+news_size    <- file.size(news_file) / (2^20)
 twitter_size <- file.size(twitter_file) / (2^20)
-
+ 
 #' Read the data files
 blogs   <- readLines(blogs_file, skipNul = TRUE)
 news    <- readLines(news_file, skipNul = TRUE)
 twitter <- readLines(twitter_file, skipNul = TRUE) 
 
 #' Number of Lines per file
-blogs_lines <- length(blogs)
-news_lines  <- length(news)
+blogs_lines   <- length(blogs)
+news_lines    <- length(news)
 twitter_lines <- length(twitter)
-total_lines  <- blogs_lines + news_lines + twitter_lines
+total_lines   <- blogs_lines + news_lines + twitter_lines
 
 #' Distibution of characters per line, by file
-blogs_nchar <- nchar(blogs)
-news_nchar  <- nchar(news)
+blogs_nchar   <- nchar(blogs)
+news_nchar    <- nchar(news)
 twitter_nchar <- nchar(twitter)
+
 boxplot(blogs_nchar, news_nchar, twitter_nchar, log = "y",
         names = c("blogs", "news", "twitter"),
         ylab = "log(Number of Characters)", xlab = "File Name")
-title("Comparing Distributions of Chracters per Line")
+        title("Comparing Distributions of Chracters per Line")
 
 #' Max characters in a line, by file (longest line)
-blogs_nchar_max <- max(blogs_nchar)
-news_nchar_max  <- max(news_nchar)
+blogs_nchar_max   <- max(blogs_nchar)
+news_nchar_max    <- max(news_nchar)
 twitter_nchar_max <- max(twitter_nchar)
 
 #' Median characters per file
-blogs_nchar_med <- median(blogs_nchar)
-news_nchar_med  <- median(news_nchar)
+blogs_nchar_med   <- median(blogs_nchar)
+news_nchar_med    <- median(news_nchar)
 twitter_nchar_med <- median(twitter_nchar)
 
 #' Total characters per file
-blogs_nchar_sum <- sum(blogs_nchar)
-news_nchar_sum  <- sum(news_nchar)
+blogs_nchar_sum   <- sum(blogs_nchar)
+news_nchar_sum    <- sum(news_nchar)
 twitter_nchar_sum <- sum(twitter_nchar)
 
 #' Create summary of repo stats
-repo_summary <- data.frame(file_names = c("blogs", "news", "twitter"),
-                           file_size  = c(blogs_size, news_size, twitter_size),
-                           file_lines = c(blogs_lines, news_lines, twitter_lines),
-                           #nchar_max  = c(blogs_nchar_max, news_nchar_max, twitter_nchar_max),
-                           #nchar_med =  c(blogs_nchar_med, news_nchar_med, twitter_nchar_med),
+repo_summary <- data.frame(f_names = c("blogs", "news", "twitter"),
+                           f_size  = c(blogs_size, news_size, twitter_size),
+                           f_lines = c(blogs_lines, news_lines, twitter_lines),
+                           nchar_max  = c(blogs_nchar_max, news_nchar_max, twitter_nchar_max),
+                           nchar_med =  c(blogs_nchar_med, news_nchar_med, twitter_nchar_med),
                            nchar_sum =  c(blogs_nchar_sum, news_nchar_sum, twitter_nchar_sum))
 
 
 repo_summary <- repo_summary %>% mutate(pct_nchar = round(nchar_sum/sum(nchar_sum), 2))
-repo_summary <- repo_summary %>% mutate(pct_lines = round(file_lines/sum(file_lines), 2))
+repo_summary <- repo_summary %>% mutate(pct_lines = round(f_lines/sum(f_lines), 2))
 kable(repo_summary)
 
 #' ## 2. Sample the data and save the sample
 #' 
 #' Compute sample sizes in terms of lines
-sample_pct = 0.01
+sample_pct = 0.05
+set.seed(1001)
 blogs_size <- blogs_lines * sample_pct
 news_size  <- news_lines * sample_pct
 twitter_size <- twitter_lines * sample_pct
@@ -96,6 +101,11 @@ repo_sample = c(blogs_sample, news_sample, twitter_sample)
 
 #' Save sample
 writeLines(repo_sample, "./data/final/en_US/en_US.repo_sample.txt")
+saveRDS(repo_sample, file = "./data/final/en_US/repo_sample.RData" )
+
+########################## START HERE ############### SEE LAST TAB
+#' [Test Mining Tutorial]("https://www.hackerearth.com/fr/practice/machine-learning/advanced-techniques/text-mining-feature-engineering-r/tutorial/")
+#'
 
 #' ## 3.  Clean the sample data
 #' Use `tm` to create and clean the corpus
@@ -103,15 +113,12 @@ clean_sample <- Corpus(VectorSource(repo_sample),
                        readerControl = list(readPlain, 
                                             language = "en",
                                             load = TRUE))
-#' 
-#' Create filter for profanity...
-#' Source: [List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words]("List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/en")
-profanity <- read.table("./data/final/en_US/profanity.txt", header = FALSE, sep ="\n")
+print(as.character(clean_sample[[1]]))
 
 #' Transform sample to all lower case
 clean_sample <- tm_map(clean_sample, content_transformer(tolower))
 
-#' Remove URL's
+#' Remove URL's  
 #' Source: [R and Data Mining]("http://www.rdatamining.com/books/rdm/faq/removeurlsfromtext")
 removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
 clean_sample <- tm_map(clean_sample, content_transformer(removeURL))
@@ -120,37 +127,65 @@ clean_sample <- tm_map(clean_sample, content_transformer(removeURL))
 removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
 clean_sample <- tm_map(clean_sample, content_transformer(removeNumPunct))
 
-# #' Remove punctuaton and numbers (replaced by last call)
-# clean_sample <- tm_map(clean_sample, removePunctuation)
-#' clean_sample <- tm_map(clean_sample, removeNumbers)
+#' Create profanity filter  
+#' Source: [List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words]("List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/en")
+profanity <- read.table("./data/final/en_US/profanity.txt", header = FALSE, sep ="\n")
 
 #' Remove profanity
 clean_sample <- tm_map(clean_sample, removeWords, profanity[,1])
 
 #' Remove stopwords
 clean_sample <- tm_map(clean_sample, removeWords, stopwords("english"))
-
-#' Stem the document
-clean_sample <- tm_map(clean_sample, stemDocument)
+clean_sample <- tm_map(clean_sample, removeWords, stopwords("SMART"))
+print(as.character(clean_sample[[1]]))
 
 #' Remove Whitespace
 clean_sample <- tm_map(clean_sample, stripWhitespace)
+print(as.character(clean_sample[[1]]))
 
-#' Create Term-Document Matrix
-dtm <- TermDocumentMatrix(clean_sample)
-# m <- as.matrix(dtm)
-# v <- sort(rowSums(m),decreasing=TRUE)
-# d <- data.frame(word = names(v),freq=v)
-# head(d, 10)
-
-#' ### Operations on Document-Term Matrices
-findFreqTerms(dtm, 5)
-
+#' Save clean corpus
 saveRDS(clean_sample, file = "./data/final/en_US/clean_sample.RData" )
-saveRDS(dtm, file = "./data/final/en_US/dtm.RData")
 
-#########################
-# library(stringi)
-# clean_sample <- stringi::stri_trans_general(clean_sample, "latin-ascii")
+#' Convert to text document
+text_corpus <- tm_map(clean_sample, PlainTextDocument)
+
+#perform stemming - this should always be performed after text doc conversion
+text_corpus <- tm_map(text_corpus, stemDocument,language = "english")
+print(as.character(text_corpus[[1]]))
+text_corpus[[1]]$content
+
+#convert to document term matrix
+docterm_corpus <- DocumentTermMatrix(text_corpus)
+dim(docterm_corpus)
+
+new_docterm_corpus <- removeSparseTerms(docterm_corpus,sparse = 0.99)
+dim(new_docterm_corpus)
+
+#find frequent terms
+colS <- colSums(as.matrix(new_docterm_corpus))
+length(colS)
+doc_features <- data.table(name = attributes(colS)$names, count = colS)
+
+#most frequent and least frequent words
+doc_features[order(-count)][1:10] #top 10 most frequent words
+doc_features[order(count)][1:10] #least 10 frequent words
+
+
+ggplot(doc_features[count>5000],aes(name, count)) +
+  geom_bar(stat = "identity",fill='lightblue',color='black') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme_economist() + scale_color_economist() 
+
+# #check association of terms of top features
+# findAssocs(new_docterm_corpus, "time", corlimit = 0.5)
+# findAssocs(new_docterm_corpus, "love", corlimit = 0.5)
+# findAssocs(new_docterm_corpus, "day",  corlimit = 0.5)
+
+library(wordcloud)
+wordcloud(names(colS), colS, min.freq = 500, 
+          colors = brewer.pal(6, 'Dark2'), random.order = FALSE)  
+
+wordcloud(names(colS), colS, min.freq = 2000, 
+          colors = brewer.pal(6, 'Dark2'), random.order = FALSE)  
 
 
