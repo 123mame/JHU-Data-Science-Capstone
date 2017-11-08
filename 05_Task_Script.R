25da249710311f6a4b812a86ac631b602b8ed81d
Mark Blackmore <mblackmo@yahoo.com>
2017-11-08 09:02:37 -0800

    Add functions

--- 05_Task_Script.R
+++ 05_Task_Script.R
@@ -17,9 +17,40 @@ bi_words <- readRDS("./clean_repos/bi_words.rds")
 tri_words  <- readRDS("./clean_repos/tri_words.rds")
 quad_words <- readRDS("./clean_repos/quad_words.rds")
 
+#+ functions
+bigram <- function(input_words, input_count){
+                    filter(bi_words, 
+                          word1==input_words[input_count]) %>% 
+                    top_n(1, n) %>%
+                    filter(row_number() == 1L) %>%
+                    select(num_range("word", input_count+1)) %>%
+                    as.character()
+}
+
+trigram <- function(input_words, input_count){
+                    filter(tri_words, 
+                            word1==input_words[input_count-1], 
+                            word2==input_words[input_count])  %>% 
+                    top_n(1, n) %>%
+                    filter(row_number() == 1L) %>%
+                    select(num_range("word", input_count+1)) %>%
+                    as.character()
+}
+
+quadgram <- function(input_words, input_count){
+                    filter(quad_words, 
+                            word1==input_words[input_count-2], 
+                            word2==input_words[input_count-1], 
+                            word3==input_words[input_count])  %>% 
+                    top_n(1, n) %>%
+                    filter(row_number() == 1L) %>%
+                    select(num_range("word", input_count+1)) %>%
+                    as.character()
+}
+
 
 #' User Input
-input <- data_frame(text = c("an"))
+input <- data_frame(text = c("in case of"))
 
 #' Logic to Predict
 input_count <- str_count(input, boundary("word"))
@@ -27,32 +58,15 @@ input_words <- unlist(str_split(input, boundary("word")))
 
 
 if (input_count == 1) {
-  filter(bi_words, 
-         word1==input_words[input_count]) %>% 
-    top_n(1, n) %>%
-    filter(row_number() == 1L) %>%
-    select(num_range("word", input_count+1)) %>%
-    as.character()
+    bigram(input_words, input_count)
+  
 } else {
   
   if (input_count == 2){
-    filter(tri_words, 
-           word1==input_words[input_count-1], 
-           word2==input_words[input_count])  %>% 
-      top_n(1, n) %>%
-      filter(row_number() == 1L) %>%
-      select(num_range("word", input_count+1)) %>%
-      as.character()
+      trigram(input_words, input_count)
     
   } else {    
-    filter(quad_words, 
-           word1==input_words[input_count-2], 
-           word2==input_words[input_count-1], 
-           word3==input_words[input_count])  %>% 
-      top_n(1, n) %>%
-      filter(row_number() == 1L) %>%
-      select(num_range("word", input_count+1)) %>%
-      as.character()
+      quadgram(input_words, input_count)
   }
 }
 
