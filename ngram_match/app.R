81da75e633594dca2105f0c19d6ad3fc25d4dee5
Mark Blackmore <mblackmo@yahoo.com>
2017-11-18 10:04:14 -0800

    Working app with descriptions, instructionns, link to source code.

--- ngram_match/app.R
+++ ngram_match/app.R
@@ -21,16 +21,18 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("Text Prediction Model"),
-   p("A Shiny app that takes as input a phrase (multiple words) in a text box input and outputs a prediction of the next word."),
+   p("This app that takes an input phrase (multiple words) in a text box and outputs a prediction of the next word."),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
         h3("Instructions"), 
-        helpText("This app predicts."),
         h5("1. Enter a word or words in the text box."),
         h5("2. The prediction prints below it in blue."),
-        h5("3. A question mark means no prediction, typically do to input mis-spelling")
+        h5("3. No need to hit enter of submit."),
+        h5("4. A question mark means no prediction, typically do to input mis-spelling"),
+        br(),
+        a("Source Code", href = "https://github.com/mark-blackmore/JHU-Data-Science-Capstone/tree/master/ngram_match")
       ),
       
       # Show a plot of the generated distribution
